21f77cad628e94c8f7ecebed5efba05626064ce2
Sergei Dmitriev <iserdmi@gmail.com>
2025-09-16 01:04:51 +0400

    fix: fix ci --skip-test

--- .releaserc.mjs
+++ .releaserc.mjs
@@ -2,32 +2,31 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 /** biome-ignore-all lint/suspicious/noConsole: <xzc> */
-export default async () => {
-  // biome-ignore lint/style/noProcessEnv: <it is ok, here>
-  const branch = process.env.GITHUB_REF?.replace('refs/heads/', '') || ''
-  console.log(345345345, branch)
-  // biome-ignore lint/style/noProcessEnv: <xc>
-  console.log(857856856865, process.env.GITHUB_REF)
 
-  const basePlugins = [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    '@semantic-release/npm',
-    '@semantic-release/github',
-  ]
+// biome-ignore lint/style/noProcessEnv: <it is ok, here>
+const branch = process.env.GITHUB_REF?.replace('refs/heads/', '') || ''
+console.log(345345345, branch)
+// biome-ignore lint/style/noProcessEnv: <xc>
+console.log(857856856865, process.env.GITHUB_REF)
 
-  const gitPlugin = [
-    '@semantic-release/git',
-    {
-      assets: ['package.json', 'CHANGELOG.md'],
-      // biome-ignore lint/suspicious/noTemplateCurlyInString: <it is ok, here>
-      message: 'chore(release): ${nextRelease.version} --skip-ci',
-    },
-  ]
+const basePlugins = [
+  '@semantic-release/commit-analyzer',
+  '@semantic-release/release-notes-generator',
+  '@semantic-release/changelog',
+  '@semantic-release/npm',
+  '@semantic-release/github',
+]
 
-  return {
-    branches: [{ name: 'main' }, { name: 'next', channel: 'next', prerelease: true }],
-    plugins: branch === 'main' ? [...basePlugins, gitPlugin] : basePlugins,
-  }
+const gitPlugin = [
+  '@semantic-release/git',
+  {
+    assets: ['package.json', 'CHANGELOG.md'],
+    // biome-ignore lint/suspicious/noTemplateCurlyInString: <it is ok, here>
+    message: 'chore(release): ${nextRelease.version} --skip-ci',
+  },
+]
+
+export default {
+  branches: [{ name: 'main' }, { name: 'next', channel: 'next', prerelease: true }],
+  plugins: branch === 'main' ? [...basePlugins, gitPlugin] : basePlugins,
 }
