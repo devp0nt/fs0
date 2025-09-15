804b40f72cc6ac9190723ac0cdd4eeba33ab0e0d
Sergei Dmitriev <iserdmi@gmail.com>
2025-09-16 01:02:28 +0400

    fix: fix ci --skip-test

--- .releaserc.mjs
+++ .releaserc.mjs
@@ -1,9 +1,13 @@
 /**
  * @type {import('semantic-release').GlobalConfig}
  */
+/** biome-ignore-all lint/suspicious/noConsole: <xzc> */
 export default async () => {
   // biome-ignore lint/style/noProcessEnv: <it is ok, here>
   const branch = process.env.GITHUB_REF?.replace('refs/heads/', '') || ''
+  console.log(345345345, branch)
+  // biome-ignore lint/style/noProcessEnv: <xc>
+  console.log(857856856865, process.env.GITHUB_REF)
 
   const basePlugins = [
     '@semantic-release/commit-analyzer',
