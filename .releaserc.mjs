e0f2b55c74a8d92b667ef0c242da67d58aafa843
Sergei Dmitriev <iserdmi@gmail.com>
2025-09-16 00:49:27 +0400

    fix: fix ci

--- .releaserc.mjs
+++ .releaserc.mjs
@@ -1,22 +1,28 @@
-export default {
-  branches: [{ name: 'main' }, { name: 'next', prerelease: true }],
-  plugins: [
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = async () => {
+  // biome-ignore lint/style/noProcessEnv: <it is ok, here>
+  const branch = process.env.GITHUB_REF?.replace('refs/heads/', '') || ''
+
+  const basePlugins = [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/github',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['package.json', 'CHANGELOG.md'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]',
-      },
-    ],
-  ],
-  pluginOptionsByBranch: {
-    next: {
-      '@semantic-release/git': false,
+  ]
+
+  const gitPlugin = [
+    '@semantic-release/git',
+    {
+      assets: ['package.json', 'CHANGELOG.md'],
+      message: 'chore(release): ${nextRelease.version} [skip ci]',
     },
-  },
+  ]
+
+  return {
+    branches: [{ name: 'main' }, { name: 'next', prerelease: true }],
+    plugins: branch === 'main' ? [...basePlugins, gitPlugin] : basePlugins,
+  }
 }
