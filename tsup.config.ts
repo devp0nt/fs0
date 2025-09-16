508ffd2007420e2141c6b2e243651988c1e8b129
Sergei Dmitriev <iserdmi@gmail.com>
2025-09-16 15:38:34 +0400

    fix: update build scripts to include all source files

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/**/*', '!src/**/*.test.*'],
   outDir: 'dist',
   format: ['cjs', 'esm'],
   dts: true,
