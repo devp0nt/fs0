a7a6ecac5c2372e1e3c7ffc40d06bcdb16f5ac84
Sergei Dmitriev <iserdmi@gmail.com>
2025-09-18 03:44:47 +0400

    refactor: restructure tsup configuration for improved clarity and maintainability, consolidating settings into a general object and enhancing entry format options

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,23 +1,29 @@
-import { defineConfig } from 'tsup'
+import { defineConfig, type Options } from 'tsup'
 
-export default defineConfig({
-  entry: ['src', '!src/**/*.test.*'],
+const general = {
   outDir: 'dist',
-  format: ['cjs', 'esm'],
   dts: true,
   splitting: false,
   sourcemap: true,
-  clean: true,
   minify: false,
   target: 'es2022',
-  outExtension({ format }) {
-    return {
-      js: format === 'cjs' ? '.cjs' : '.js',
-    }
-  },
   external: ['bun:test'],
   treeshake: false,
   bundle: false,
   platform: 'node',
   tsconfig: './tsconfig.build.json',
-})
+} satisfies Options
+
+export default defineConfig([
+  {
+    ...general,
+    clean: true,
+    entry: ['src', '!src/**/*.test.*'],
+    format: ['cjs', 'esm'],
+    outExtension({ format }) {
+      return {
+        js: format === 'cjs' ? '.cjs' : '.js',
+      }
+    },
+  },
+])
